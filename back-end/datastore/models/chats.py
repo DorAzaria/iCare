85fb39325b95b661519cbdd1d85a6038257f78b8
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2022-07-27 19:56:37 +0300

    del

--- back-end/datastore/models/chats.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Chat(models.Model):
-    application_id = models.IntegerField()
-
-class Message(models.Model):
-    chat_id = models.IntegerField()
-    author_id = models.IntegerField()
-    contents = models.TextField()
