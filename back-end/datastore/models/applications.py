c398698611944fae826785284e03ea8510dd8f4d
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2022-08-16 16:43:58 +0300

    deleted

--- back-end/datastore/models/applications.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Application(models.Model):
-    from_id = models.IntegerField()
-    job_id = models.IntegerField()
-    cover_letter = models.TextField()
--- back-end/datastore/models/replys.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Reply(models.Model):
-    comment_id = models.IntegerField()
-    from_id = models.IntegerField()
-    description = models.TextField()
-    enabled = models.BooleanField()
-    post_time = models.BigIntegerField()
-    
